e5b6f33069434ef8a7a69de3cf842587e0b0ebf3
Brian Peterson <bepetersn@gmail.com>
2013-11-29 23:19:30 -0600

    stored some basic info on each stage in general context

--- tarbell_config.py
+++ tarbell_config.py
@@ -8,7 +8,7 @@ Tarbell project configuration
 NAME = "crime_punishment"
 
 # Descriptive title of project
-TITLE = "Crime and Punishment"
+TITLE = "Crime, Punishment and Discretion in Chicago"
 
 # Google spreadsheet key
 #SPREADSHEET_KEY = "None"
@@ -28,12 +28,28 @@ TEMPLATE_REPO_URL = "https://github.com/newsapps/tarbell-template"
 # Default template variables
 DEFAULT_CONTEXT = {
 
-    'name': 'crime_punishment',
-    'title': 'Crime and Punishment Stuff1212',
-    'call_text': unicode(open('copy/call.md').read()),
-    'police_text': unicode(open('copy/police.md').read()),
-    'charges_text': unicode(open('copy/charges.md').read()),
-    'courts_text': unicode(open('copy/courts.md').read()),
+    'name': u'crime_punishment',
+    'title': u'Crime, Punishment and Discretion in Chicago',
+    'call_page': {
+    	'title': u'Call for Service',
+    	'intro': u"There are two potential starting points for an individual's contact with the law. The first is that a call for service goes out to a 911 operator at the [Office of Emergency Management and Communications](https://www.cityofchicago.org/city/en/depts/oem.html) (OEMC). After the operator categorizes the call based on the information they received, it is dispatched to a police officer.",
+    	'text': unicode(open('copy/call.md').read())
+    },
+    'police_page': {
+    	'title': u'Police Response',
+    	'intro': u'',
+    	'text': unicode(open('copy/police.md').read())
+    },
+    'charges_page': {
+    	'title': u"State's Attorney's Office",
+    	'intro': u'',
+    	'text': unicode(open('copy/charges.md').read())
+    },
+    'courts_page': {
+    	'title': u'Courts',
+    	'intro': u'',
+    	'text': unicode(open('copy/courts.md').read())
+    }
 
 }
 
