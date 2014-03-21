73e2ac3accd7504a7a86266cfc12120e9edbafc8
Brian Peterson <bepetersn@gmail.com>
2014-03-21 15:07:20 -0500

    customized the sizes of the expandable sections

--- js/app.js
+++ js/app.js
@@ -16,7 +16,25 @@ $(document).ready(function() {
     $('div.featured-image-wrapper h1').fadeIn(1000); 
   }, 300);
   
-  $('div.expandable').expander({
+  $('#callforservice-foiable').expander({
+    slicePoint: 400
+  });
+  $('#incidents-available').expander({
+    slicePoint: 100
+  });
+  $('#incidents-foiable').expander({
     slicePoint: 200
+  });
+  $('#incidents-unavailable').expander({
+    slicePoint: 200
+  });
+  $('#jail-foiable').expander({
+    slicePoint: 600
+  });
+  $('#court-unavailable').expander({
+    slicePoint: 700
+  });
+  $('#prison-available').expander({
+    slicePoint: 100
   });
 });
